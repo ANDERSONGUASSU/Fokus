422894be28ee83fa509e0e7205d23a9cefa37dd7
AndersonGuassu <andersonguassu@hotmail.com>
2024-01-05 21:54:36 -0300

    alterando imagens

--- script.js
+++ script.js
@@ -2,13 +2,19 @@ const html = document.querySelector('html')
 const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
+const banner = document.querySelector('.app__image')
 
 focoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco')
+    banner.setAttribute('src', '/img/foco.png')
 })
 curtoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto')
+    html.setAttribute('data-contexto', 'short')
+    banner.setAttribute('src', '/img/short.png')
+
 })
 longoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo')
+    html.setAttribute('data-contexto', 'long')
+    banner.setAttribute('src', '/img/long.png')
+
 })
--- script1.js
+++ script1.js
@@ -1,9 +1,11 @@
 const html =  document.querySelector('html');
 const buttons = document.querySelectorAll('.app__card-button');
+const banner = document.querySelector('.app__image')
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         const contexto = button.dataset.contexto;
         html.setAttribute('data-contexto', contexto)
+        banner.setAttribute('src', `/img/${contexto}.png`)
     })
 })
